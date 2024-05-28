fbb45c5041f611d9cd9b7b7335ae928e0f0e578e
g-neti <83729743+g-neti@users.noreply.github.com>
2024-05-28 14:08:37 +0100

    Update streamlit_app.py
    Move FruityVice response code to the 'for fruit_chosen' loop.

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,6 +2,7 @@
 import streamlit as st
 from snowflake.snowpark.functions import col
 from snowflake.snowpark.context import get_active_session
+import requests
 
 # Write directly to the app
 st.title(":cup_with_straw: Customize Your Smoothie :cup_with_straw:")
@@ -31,6 +32,9 @@ if ingredient_list:
     ingredients_string = '' 
     for fruit_chosen in ingredient_list:
         ingredients_string += fruit_chosen + ' '
+        # Display fruityvice nutrition information: 
+        fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+        fv_df = st.dataframe(data = fruityvice_response.json(), use_container_width = True)
     st.text(ingredients_string)
     
     # Create an insert statement and use in SQL editor:
@@ -46,8 +50,4 @@ if ingredient_list:
         session.sql(my_insert_stmt).collect()
         st.success('Your Smoothie is ordered, '+name_on_order +'!', icon="✅")
 
-# New section to display fruityvice nutrition information
-import requests
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-# st.text(fruityvice_response.json())
-fv_df = st.dataframe(data = fruityvice_response.json(), use_container_width = True)
+
